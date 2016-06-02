877695367e1b3078af60ea3af3ef266c6ce7da7a
Artem Klimov <artem@klimov.com>
2016-06-02 17:37:23 +0300

    Поправил верхнее меню

--- frontend/widgets/sportsMenu/views/default.php
+++ frontend/widgets/sportsMenu/views/default.php
@@ -1,19 +1,19 @@
-<?php
-/**
- * @var $sports []
- */
-?>
-
-<ul class="nav navbar-nav hidden-sm-down">
-    <li class="nav-item">
-        <i class="fa fa-bars menu-trigger fa-2x" aria-hidden="true"></i>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link nav-logo" href="/">TriRussia.ru</a>
-    </li>
-    <?php foreach ($sports as $sport) { ?>
-        <li class="nav-item">
-            <a class="nav-link" href="<?= \yii\helpers\Url::to(['/', 'sport' => $sport->url, ])?>"><?= $sport->label; ?></a>
-        </li>
-    <?php } ?>
-</ul>
+<nav class="nav nav-inline c-nav">
+	<div class="container">
+		<div class="pull-left">
+			<ul class="list-inline">
+				<li class="nav-item"><i class="fa fa-bars fa-lg nav-hamb menu-trigger" aria-hidden="true"></i></li>
+				<li class="nav-item m-l-2 m-t-1 m-b-1 m-r-1"><a href="/"><img src="/img/logo.png"></a></li>
+				<?php foreach ($sports as $sport) { ?>
+					<li class="nav-item"><a href="<?= \yii\helpers\Url::to(['/', 'sport' => $sport->url, ])?>" class="nav-link"><?= $sport->label; ?></a></li>
+				<?php } ?>
+				<li class="nav-item"><a href="/magazine" class="nav-link">Журнал</a></li>
+				<li class="nav-item"><a href="/shop" class="nav-link">Магазин</a></li>
+			</ul>
+		</div>
+		<div class="pull-right">
+			<button class="btn btn-primary btn-sm m-t-1" data-toggle="modal" data-target="#openUser">Войти</button>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</nav>
