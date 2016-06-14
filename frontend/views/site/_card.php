278500953687514c427a6d941d9a1677cda5c352
Artem Klimov <artem@klimov.com>
2016-06-14 18:52:34 +0300

    Организатор в карточке

--- frontend/views/site/_card.php
+++ frontend/views/site/_card.php
@@ -60,7 +60,7 @@ die();*/
                         <?php if ($race->organizer->image_id) { ?>
                             <img src="<?= FPM::originalSrc($race->organizer->image_id)?>" class="card-organizer-logo">
                         <?php } else { ?>
-                            <span class="PTSerif"><i><?= $race->organizer; ?></i></span>
+                            <span class="PTSerif"><i><?= $race->organizer->organizer_id; ?></i></span>
                         <?php }  ?>
                     </div>
                     <div class="pull-right">
