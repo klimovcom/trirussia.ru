8350eceee3ff36357a002fc969aec88d1c79eee6
Artem Klimov <artem@klimov.com>
2016-06-15 15:06:36 +0300

    404 страница

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,19 +9,13 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
+<div class="container">
+	<h1 class="m-t-3 m-b-3 text-xs-center">Опаньки: <?= Html::encode($this->title) ?></h1>
+	<div class="alert alert-danger text-xs-center">
+		<?= nl2br(Html::encode($message)) ?>
+	</div>
+	<p class="text-xs-center">
+		Видимо, что-то пошло не так. Но вы не расстраивайтесь: перейдите на <a href="/" class="underline">главную страницу</a>. Если ошибка будет повторяться, то обязательно напишите нам на <a href="mailto:artem@klimov.com" class="underline">artem@klimov.com</a>. Удачных стартов!
+	</p>
 </div>
