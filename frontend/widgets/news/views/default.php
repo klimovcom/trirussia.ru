0d44c276973f7ef0415e35b2afcbd672da0ae00d
fkbr1993 <fkbr1993@yandex.ru>
2016-07-26 23:11:17 +0300

    some changes

--- frontend/widgets/news/views/default.php
+++ frontend/widgets/news/views/default.php
@@ -18,7 +18,7 @@ $firstNew = array_shift($news);
             <hr>
             <h6 class="magazine-caption news">Новости</h6>
             <h2><a href="#" class="no-underline"><?= $firstNew->label; ?></a></h2>
-            <span class="text-muted small"><?= $firstNew->promo; ?></span>
+            <span class="text-muted small">><?= Yii::$app->formatter->asDate($firstNew->created, 'd MMMM yyyy') . ' г.';; ?></span>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
             <div class="row">
@@ -29,7 +29,7 @@ $firstNew = array_shift($news);
                                 <hr>
                                 <h6 class="magazine-caption news">Новости</h6>
                                 <h5><a href="#" class="no-underline"><?= $new->label; ?></a></h5>
-                                <span class="text-muted small"><?= Yii::$app->formatter->asDate($new->created, 'd MMMM yyyy') . ' г.';; ?></span>
+                                <span class="text-muted small"><?= Yii::$app->formatter->asDate($new->created, 'd MMMM yyyy') . ' г.'; ?></span>
                             </li>
                         </div>
                     <?php } ?>
