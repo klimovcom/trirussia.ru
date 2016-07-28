f82d2c4eb3c0b021ae59d0836694b73e616985e9
Artem Klimov <artem@klimov.com>
2016-07-28 21:34:51 +0300

    Самые ожидаемые соревнования

--- frontend/widgets/mostPopularRaces/views/default.php
+++ frontend/widgets/mostPopularRaces/views/default.php
@@ -12,7 +12,7 @@ use metalguardian\fileProcessor\helpers\FPM;
    <?php foreach ($races as $race) {?>
        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3 m-t-1">
            <a href="<?= Url::to(['/race/default/view', 'url' => $race->url,]) ?>">
-               <img src="<?= FPM::originalSrc($race->main_image_id);?>" class="img-fluid">
+               <img src="<?= FPM::originalSrc($race->main_image_id);?>" class="img-fluid most-wanted">
            </a>
            <h5 class="m-t-1">
                <a href="<?= Url::to(['/race/default/view', 'url' => $race->url,]) ?>" class="no-underline">
