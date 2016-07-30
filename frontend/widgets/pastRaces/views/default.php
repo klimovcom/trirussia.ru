442d3ebc2ffdbc8513993651d634f8ccf856b30f
Artem Klimov <artem@klimov.com>
2016-07-30 11:17:51 +0300

    Поправил последние законченные гонки

--- frontend/widgets/pastRaces/views/default.php
+++ frontend/widgets/pastRaces/views/default.php
@@ -7,7 +7,7 @@
 <h4 class="PTSerif"><i>Прошедшие соревнования</i></h4>
 <div class="row">
     <?php foreach ($pastRaces as $race) {?>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3 race-block">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
             <hr>
             <h6 class="sport-caption tri"><?= $race->sport->label; ?></h6>
             <h5>
