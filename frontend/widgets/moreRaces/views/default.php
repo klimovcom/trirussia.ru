b8a1511feae8f876d975271226bc7fb2f2eb65fa
Artem Klimov <artem@klimov.com>
2016-08-22 09:09:50 +0300

    Всё в дательный падеж

--- frontend/widgets/moreRaces/views/default.php
+++ frontend/widgets/moreRaces/views/default.php
@@ -7,7 +7,7 @@ use yii\helpers\Url;
 ?>
 <div class="card-block border-run">
     <h4>
-	    <a href="#" class="underline-black"><?= $this->context->model->sport->label; ?></a> очень популярен. Посмотрите другие соревнования в категории <?= $this->context->model->sport->label; ?>:
+	    Посмотрите другие соревнования по <?= \sport\models\Sport::getCurrentSportLabel('дательный');?>:
     </h4>
     <ul class="list-unstyled m-t-1">
         <?php /** @var $race \race\models\Race */?>
@@ -23,10 +23,10 @@ use yii\helpers\Url;
 
     <ul class="list-inline m-t-1 m-b-0">
         <li class="list-inline-item m-r-2">
-            <a href="<?= Url::to(['/', 'sport' => $this->context->model->sport->url, 'country' => 'Россия']); ?>" class="underline small">Все соревнования в стране <?= $race->country; ?> в категории <?= $this->context->model->sport->label; ?></a>
+            <a href="<?= Url::to(['/', 'sport' => $this->context->model->sport->url, 'country' => 'Россия']); ?>" class="underline small">Все соревнования в стране <?= $race->country; ?> по <?= \sport\models\Sport::getCurrentSportLabel('дательный');?></a>
         </li>
         <li class="list-inline-item m-r-2">
-            <a href="<?= Url::to(['/', 'sport' => $this->context->model->sport->url])?>" class="underline small">Все соревнования в мире в категории <?= $this->context->model->sport->label; ?></a>
+            <a href="<?= Url::to(['/', 'sport' => $this->context->model->sport->url])?>" class="underline small">Все соревнования в мире по <?= \sport\models\Sport::getCurrentSportLabel('дательный');?></a>
         </li>
     </ul>
 </div>
