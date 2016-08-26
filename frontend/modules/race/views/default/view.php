8c8e8056ea97cdd3958c5df756b5e0be2cc508b8
Artem Klimov <artem@klimov.com>
2016-08-26 13:31:54 +0300

    Добавил код AdRiver

--- frontend/modules/race/views/default/view.php
+++ frontend/modules/race/views/default/view.php
@@ -144,13 +144,41 @@ $quest = Yii::$app->user->isGuest ? 'data-toggle="modal" data-target="#openUser"
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 sidebar">
         <div class="theiaStickySidebar">
             <div class="ad-sidebar text-xs-center">
-                <img src="https://s-media-cache-ak0.pinimg.com/736x/2d/82/a6/2d82a6a6be76603d79a263f05ee96ac8.jpg">
+				<!--  AdRiver code START. Type:300x250 Site: trirussia PZ: 0 BN: 1 -->
+				<script type="text/javascript">
+				var RndNum4NoCash = Math.round(Math.random() * 1000000000);
+				var ar_Tail='unknown'; if (document.referrer) ar_Tail = escape(document.referrer);
+				document.write(
+				'<iframe src="' + ('https:' == document.location.protocol ? 'https:' : 'http:') + '//ad.adriver.ru/cgi-bin/erle.cgi?'
+				+ 'sid=201788&bn=1&target=blank&w=300&h=600&bt=40&pz=0&rnd=' + RndNum4NoCash + '&tail256=' + ar_Tail
+				+ '" frameborder=0 vspace=0 hspace=0 width=300 height=600 marginwidth=0'
+				+ ' marginheight=0 scrolling=no></iframe>');
+				</script>
+				<noscript>
+				<a href="//ad.adriver.ru/cgi-bin/click.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" target=_blank>
+				<img src="//ad.adriver.ru/cgi-bin/rle.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" alt="-AdRiver-" border=0 width=300 height=600></a>
+				</noscript>
+				<!--  AdRiver code END  -->
             </div>
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 hidden new-sidebar">
         <div class="ad-sidebar text-xs-center">
-            <img src="https://s-media-cache-ak0.pinimg.com/736x/2d/82/a6/2d82a6a6be76603d79a263f05ee96ac8.jpg">
+			<!--  AdRiver code START. Type:300x250 Site: trirussia PZ: 0 BN: 1 -->
+			<script type="text/javascript">
+			var RndNum4NoCash = Math.round(Math.random() * 1000000000);
+			var ar_Tail='unknown'; if (document.referrer) ar_Tail = escape(document.referrer);
+			document.write(
+			'<iframe src="' + ('https:' == document.location.protocol ? 'https:' : 'http:') + '//ad.adriver.ru/cgi-bin/erle.cgi?'
+			+ 'sid=201788&bn=1&target=blank&w=300&h=600&bt=40&pz=0&rnd=' + RndNum4NoCash + '&tail256=' + ar_Tail
+			+ '" frameborder=0 vspace=0 hspace=0 width=300 height=600 marginwidth=0'
+			+ ' marginheight=0 scrolling=no></iframe>');
+			</script>
+			<noscript>
+			<a href="//ad.adriver.ru/cgi-bin/click.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" target=_blank>
+			<img src="//ad.adriver.ru/cgi-bin/rle.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" alt="-AdRiver-" border=0 width=300 height=600></a>
+			</noscript>
+			<!--  AdRiver code END  -->
         </div>
         <div class="ad-sidebar text-xs-center">
             <img src="http://lightnup.ph/wp-content/uploads/2015/02/2XU2-300x250.jpg">
