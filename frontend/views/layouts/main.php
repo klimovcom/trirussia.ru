7867d04f54092b2d6d564d7dc97ecbb542e99781
Artem Klimov <artem@klimov.com>
2016-08-28 14:25:13 +0300

    Откатил назад

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -190,48 +190,6 @@ AppAsset::register($this);
     });
 </script>
 
-<!-- Yandex.Metrika counter -->
-<script src="https://mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
-<script type="text/javascript">
-try {
-	var yaCounter26019216 = new Ya.Metrika({
-		id:26019216,
-		clickmap:true,
-		trackLinks:true,
-		accurateTrackBounce:true,
-		webvisor:true
-	});
-} catch(e) { }
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/26019216" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-
-<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-16018298-20', 'auto');
-	ga('send', 'pageview');
-</script>
-
-<!-- Rating@Mail.ru counter -->
-<script type="text/javascript">
-	var _tmr = _tmr || [];
-	_tmr.push({id: "2662254", type: "pageView", start: (new Date()).getTime()});
-	(function (d, w, id) {
-		if (d.getElementById(id)) return;
-		var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-		ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
-		var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-		if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-	})(document, window, "topmailru-code");
-</script>
-<noscript><div style="position:absolute;left:-10000px;">
-<img src="//top-fwz1.mail.ru/counter?id=2662254;js=na" style="border:0;" height="1" width="1" alt="Рейтинг@Mail.ru" />
-</div></noscript>
-<!-- //Rating@Mail.ru counter -->
-
 <?php $this->endBody() ?>
 </body>
 </html>
