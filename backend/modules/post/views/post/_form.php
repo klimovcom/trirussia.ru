f99c735ffcfc97598f0536f6966d32ba44a7b1f3
kaytus <neoname0q@gmail.com>
2016-10-13 09:30:12 +0400

    url from name

--- backend/modules/post/views/post/_form.php
+++ backend/modules/post/views/post/_form.php
@@ -41,9 +41,7 @@ use yii\widgets\ActiveForm;
                 ?>
             </div>
         </div>
-        <input type="range" oninput="myFun)">
         <?= $form->field($model, 'label')->textInput(['maxlength' => true, 'class' => 'form-control w850 ']) ?>
-
         <div class="row">
             <div class="col-md-6">
                 <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
