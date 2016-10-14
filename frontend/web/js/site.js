0a231919acb60aa7060aef8159c7af22ad7a8489
kaytus <neoname0q@gmail.com>
2016-10-14 17:36:18 +0400

    corrected

--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -320,14 +320,14 @@ jQuery(document).ready(function($){
     //hide or show the "back to top" link
     $(window).scroll(function(){
         if ( $(this).scrollTop() > offset ) {
-            console.log(1);
+
             $('.cd-top').addClass('cd-is-visible')
         } else {
-            console.log(2);
+
             $('.cd-top').removeClass('cd-is-visible cd-fade-out')
         }
         if( $(this).scrollTop() > offset_opacity ) {
-            console.log(3);
+
             $('.cd-top').addClass('cd-fade-out');
         }
     });
