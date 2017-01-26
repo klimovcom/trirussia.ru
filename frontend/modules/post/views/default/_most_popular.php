6f5472a26e64bc6c04dca1c053e7c698c724bef2
ritor <riiitor@gmail.com>
2017-01-26 12:49:01 +0300

    В середине статьи в журнале сделать перебивку "почитайте ещё"

---
+++ frontend/modules/post/views/default/_most_popular.php
@@ -0,0 +1,25 @@
+<?php
+use yii\helpers\Html;
+?>
+
+<div class="card">
+    <div class="card-block border-gold">
+        <h4 class="PTSerif"><i>Ещё интересно</i></h4>
+        <hr>
+        <div class="row">
+            <ul class="flex-container">
+                <?php foreach ($posts as $post):?>
+                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                        <li class="flex-item">
+                            <?= Html::a(
+                                Html::img(\metalguardian\fileProcessor\helpers\FPM::originalSrc($post->image_id), ['class' => 'img-fluid most-wanted', 'alt' => $post->label, 'title' => $post->label]),
+                                ['/post/default/view', 'url' => $post->url]
+                            );?>
+                            <h5 class="m-t-1"><?= Html::a($post->label, ['/post/default/view', 'url' => $post->url], ['class' => 'no-underline']);?></h5>
+                        </li>
+                    </div>
+                <?php endforeach;?>
+            </ul>
+        </div>
+    </div>
+</div>
