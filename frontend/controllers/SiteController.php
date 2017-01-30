6f252e19b6296132f90773ca074338b79421be56
ritor <riiitor@gmail.com>
2017-01-30 07:34:30 +0300

    debug

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -108,6 +108,7 @@ class SiteController extends Controller
         }
 
         $attributes = $client->getUserAttributes();
+        Yii::info(json_encode($attributes));
         //$client->setReturnUrl(\Yii::$app->request->url);
 
         if (!empty($attributes['email'])){
