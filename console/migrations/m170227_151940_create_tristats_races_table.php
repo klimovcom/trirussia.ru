14c5e60631e3e81e9105e9594de28b9e0495e9de
ritor <riiitor@gmail.com>
2017-02-27 21:57:17 +0300

    tristats migration

--- console/migrations/m170227_151940_create_tristats_races_table.php
+++ console/migrations/m170227_151940_create_tristats_races_table.php
@@ -12,6 +12,13 @@ class m170227_151940_create_tristats_races_table extends Migration
      */
     public function up()
     {
+
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('tristats_races', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull(),
@@ -25,7 +32,7 @@ class m170227_151940_create_tristats_races_table extends Migration
             'min_bike' => $this->string(),
             'min_run' => $this->string(),
             'min_finish' => $this->string(),
-        ]);
+        ], $tableOptions);
     }
 
     /**
