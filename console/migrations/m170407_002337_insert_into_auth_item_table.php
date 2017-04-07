a13fedc2bb887bfa9e318d245a999836349aebd3
ritor <riiitor@gmail.com>
2017-04-07 03:34:07 +0300

    user role

---
+++ console/migrations/m170407_002337_insert_into_auth_item_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m170407_002337_insert_into_auth_item_table extends Migration
+{
+    public $tableName = 'auth_item';
+
+    public function up()
+    {
+        $this->insert($this->tableName, [
+            'name' => 'user',
+            'type' => '1',
+            'description' => 'Пользователь',
+            'created_at' => '1487075689',
+            'updated_at' => '1487075689',
+        ]);
+
+    }
+
+    public function down()
+    {
+        return true;
+    }
+}
---
+++ console/migrations/m170407_002536_insert_into_auth_item_child_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+class m170407_002536_insert_into_auth_item_child_table extends Migration
+{
+    public $tableName = 'auth_item_child';
+
+    public function up()
+    {
+        $this->insert($this->tableName, [
+            'parent' => 'user',
+            'child' => 'coach',
+        ]);
+
+        $this->insert($this->tableName, [
+            'parent' => 'user',
+            'child' => 'race',
+        ]);
+    }
+
+    public function down()
+    {
+        return true;
+    }
+}
