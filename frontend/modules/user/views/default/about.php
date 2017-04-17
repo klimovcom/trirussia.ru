e610906c0bb69e3a07fa65624f23bee9e8caae91
ritor <riiitor@gmail.com>
2017-04-17 17:51:59 +0300

    user_info

--- frontend/modules/user/views/default/about.php
+++ frontend/modules/user/views/default/about.php
@@ -24,72 +24,70 @@ $alertArray = [
                 ]) : '';
             }
             ?>
-            <?php $form = ActiveForm::begin([
-                'class' => 'm-t-3',
-                'enableClientValidation' => true,
-                'errorCssClass' => 'has-danger',
-                'successCssClass' => 'has-success',
-                'fieldConfig' => [
-                    'options' => [
-                        'tag' => 'fieldset',
-                        'class' => 'form-group row',
+            <div class="card card-block">
+                <?php $form = ActiveForm::begin([
+                    'class' => 'm-t-3',
+                    'enableClientValidation' => true,
+                    'errorCssClass' => 'has-danger',
+                    'successCssClass' => 'has-success',
+                    'fieldConfig' => [
+                        'options' => [
+                            'tag' => 'fieldset',
+                            'class' => 'form-group row',
+                        ],
+                        'inputOptions' => [
+                            'class' => 'form-control form-control-danger',
+                        ],
+                        'labelOptions' => [
+                            'class' => 'col-sm-4 form-control-label',
+                        ],
+                        'errorOptions' => [
+                            'class' => 'form-control-feedback small'
+                        ],
+                        'template' => "{label}\n<div class=\"col-sm-8\">{input}</div>\n<div class=\"col-sm-8 col-sm-offset-4\">{hint}\n{error}</div>",
                     ],
-                    'inputOptions' => [
-                        'class' => 'form-control form-control-danger',
-                    ],
-                    'labelOptions' => [
-                        'class' => 'col-sm-4 form-control-label',
-                    ],
-                    'errorOptions' => [
-                        'class' => 'form-control-feedback small'
-                    ],
-                    'template' => "{label}\n<div class=\"col-sm-8\">{input}</div>\n<div class=\"col-sm-8 col-sm-offset-4\">{hint}\n{error}</div>",
-                ],
 
-            ]);?>
-            <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
+                ]);?>
+                <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'gender')->dropDownList(UserInfo::getGenderArray());?>
+                <?= $form->field($model, 'gender')->dropDownList(UserInfo::getGenderArray(), [
+                    'class' => 'c-select',
+                ]);?>
 
-            <?= $form->field($model, 'birthdate')->widget(\kartik\date\DatePicker::className(), [
-                'name' => 'check_issue_date',
-                'options' => ['placeholder' => 'Выберите дату '],
-                'pluginOptions' => [
-                    'format' => 'yyyy-mm-dd',
-                    'todayHighlight' => true,
-                    'weekStart' => '1',
-                ],
-                'pickerButton' => '<span class="input-group-addon kv-date-calendar" title="Выбрать дату"><i class="fa fa-calendar" aria-hidden="true"></i></span>',
-                'removeButton' => '<span class="input-group-addon kv-date-remove" title="Очистить поле"><i class="fa fa-trash" aria-hidden="true"></i></span>',
+                <?= $form->field($model, 'birthdate')->textInput([
+                    'maxlength' => true,
+                    'class' => 'form-control form-control-danger datepicker',
+                ]) ?>
 
-            ])->label(); ?>
+                <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'team')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'team')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'shirt_size')->dropDownList(UserInfo::getShirtSizeArray(), [
+                    'class' => 'c-select',
+                ]) ?>
 
-            <?= $form->field($model, 'shirt_size')->dropDownList(UserInfo::getShirtSizeArray()) ?>
+                <h3 class="m-t-1 m-b-1">Данные экстренного контакта</h3>
 
-            <h3 class="m-t-1 m-b-1">Данные экстренного контакта</h3>
+                <?= $form->field($model, 'emergency_first_name')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'emergency_first_name')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'emergency_last_name')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'emergency_last_name')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'emergency_phone')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'emergency_phone')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'emergency_relation')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'emergency_relation')->textInput(['maxlength' => true]) ?>
-
-            <div class="text-xs-right">
-                <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary btn-lg']) ?>
+                <div class="text-xs-right">
+                    <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary btn-lg']) ?>
+                </div>
+                <?php ActiveForm::end(); ?>
             </div>
-            <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
