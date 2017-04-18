aca129ed1e4eda9b9e7ed495341c19b91ccbdbba
ritor <riiitor@gmail.com>
2017-04-18 19:59:17 +0300

    race register

--- frontend/modules/race/views/default/_race_document.php
+++ frontend/modules/race/views/default/_race_document.php
@@ -5,10 +5,10 @@ use race\models\RaceFpmFile;
 
 if ($model->file->extension === 'pdf') {
     $src = '/img/download_file.png';
-    $img_class = 'embed-responsive-item m-b-1 not-fancy';
+    $img_class = 'img-fluid m-b-1 not-fancy';
 }else {
     $src = FPM::originalSrc($model->fpm_file_id);
-    $img_class = 'embed-responsive-item m-b-1';
+    $img_class = 'img-fluid m-b-1';
 }
 
 if ($model->type === RaceFpmFile::TYPE_REGULATION) {
@@ -18,6 +18,6 @@ if ($model->type === RaceFpmFile::TYPE_REGULATION) {
 }
 
 echo Html::tag('div',
-    Html::tag('div', Html::img($src, ['class' => $img_class]), ['class' => 'embed-responsive embed-responsive-16by9']) .
+    Html::tag('div', Html::tag('div', Html::img($src, ['class' => $img_class]), ['class' => 'embed-responsive-item']), ['class' => 'embed-responsive embed-responsive-16by9']) .
     Html::tag('p', Html::a($name, FPM::originalSrc($model->fpm_file_id), ['class' => 'underline', 'target' => '_blank']),['class' => 'small text-xs-center']),
     ['class' => 'col-xl-4']);
