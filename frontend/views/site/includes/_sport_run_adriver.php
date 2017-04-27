e1e221bed3389022803965867b72917a4e036b35
ritor <riiitor@gmail.com>
2017-04-27 15:49:55 +0300

    adriver run

---
+++ frontend/views/site/includes/_sport_run_adriver.php
@@ -0,0 +1,8 @@
+<!--  AdRiver code START. Type:AjaxJS Site: trirussia -->
+<div id="adriver_banner_15493707"></div>
+
+<script type="text/javascript">
+    new adriver("adriver_banner_15493707", {sid:201788, bt:52});
+</script>
+
+<!--  AdRiver code END  -->
