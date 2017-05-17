f7163d0824bf580e3309ec51dce754cb7af4ebd8
ritor <riiitor@gmail.com>
2017-05-17 16:29:19 +0300

    camps

--- frontend/modules/camp/views/default/view.php
+++ frontend/modules/camp/views/default/view.php
@@ -71,9 +71,28 @@ $price = $model->getPriceRepresentation() ? $model->getPriceRepresentation() : H
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 sidebar">
-        <div class="theiaStickySidebar">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 sidebar">
+            <div class="theiaStickySidebar">
+                <div class="ad-sidebar text-xs-center">
+                    <!--  AdRiver code START. Type:300x250 Site: trirussia PZ: 0 BN: 1 -->
+                    <script type="text/javascript">
+                        var RndNum4NoCash = Math.round(Math.random() * 1000000000);
+                        var ar_Tail='unknown'; if (document.referrer) ar_Tail = escape(document.referrer);
+                        document.write(
+                            '<iframe src="' + ('https:' == document.location.protocol ? 'https:' : 'http:') + '//ad.adriver.ru/cgi-bin/erle.cgi?'
+                            + 'sid=201788&bn=1&target=blank&w=300&h=600&bt=40&pz=0&rnd=' + RndNum4NoCash + '&tail256=' + ar_Tail
+                            + '" frameborder=0 vspace=0 hspace=0 width=300 height=600 marginwidth=0'
+                            + ' marginheight=0 scrolling=no></iframe>');
+                    </script>
+                    <noscript>
+                        <a href="//ad.adriver.ru/cgi-bin/click.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" target=_blank>
+                            <img src="//ad.adriver.ru/cgi-bin/rle.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" alt="-AdRiver-" border=0 width=300 height=600></a>
+                    </noscript>
+                    <!--  AdRiver code END  -->
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 hidden new-sidebar">
             <div class="ad-sidebar text-xs-center">
                 <!--  AdRiver code START. Type:300x250 Site: trirussia PZ: 0 BN: 1 -->
                 <script type="text/javascript">
@@ -93,66 +112,46 @@ $price = $model->getPriceRepresentation() ? $model->getPriceRepresentation() : H
             </div>
         </div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 hidden new-sidebar">
-        <div class="ad-sidebar text-xs-center">
-            <!--  AdRiver code START. Type:300x250 Site: trirussia PZ: 0 BN: 1 -->
-            <script type="text/javascript">
-                var RndNum4NoCash = Math.round(Math.random() * 1000000000);
-                var ar_Tail='unknown'; if (document.referrer) ar_Tail = escape(document.referrer);
-                document.write(
-                    '<iframe src="' + ('https:' == document.location.protocol ? 'https:' : 'http:') + '//ad.adriver.ru/cgi-bin/erle.cgi?'
-                    + 'sid=201788&bn=1&target=blank&w=300&h=600&bt=40&pz=0&rnd=' + RndNum4NoCash + '&tail256=' + ar_Tail
-                    + '" frameborder=0 vspace=0 hspace=0 width=300 height=600 marginwidth=0'
-                    + ' marginheight=0 scrolling=no></iframe>');
-            </script>
-            <noscript>
-                <a href="//ad.adriver.ru/cgi-bin/click.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" target=_blank>
-                    <img src="//ad.adriver.ru/cgi-bin/rle.cgi?sid=201788&bn=1&bt=40&pz=0&rnd=335117872" alt="-AdRiver-" border=0 width=300 height=600></a>
-            </noscript>
-            <!--  AdRiver code END  -->
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
-        <?php if($model->coord_lat && $model->coord_lon) { ?>
-            <div class="card">
-                <div class="card-block">
-                    <h4 class="card-title m-b-0">Где проходят сборы</h4>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
+            <?php if($model->coord_lat && $model->coord_lon) { ?>
+                <div class="card">
+                    <div class="card-block">
+                        <h4 class="card-title m-b-0">Где проходят сборы</h4>
+                    </div>
+                    <div id="googleMap" data-lon="<?=$model->coord_lon;?>" data-lat="<?=$model->coord_lat;?>" style="height:455px;"></div>
                 </div>
-                <div id="googleMap" data-lon="<?=$model->coord_lon;?>" data-lat="<?=$model->coord_lat;?>" style="height:455px;"></div>
+            <?php } ?>
+            <div class="card m-t-1">
+                <?= \frontend\widgets\moreRaces\MoreRaces::widget([])?>
             </div>
-        <?php } ?>
-        <div class="card m-t-1">
-            <?= \frontend\widgets\moreRaces\MoreRaces::widget([])?>
-        </div>
-        <div class="card m-t-1">
-            <div class="card-block">
-                <div id="disqus_thread"></div>
-                <script type="text/javascript">
-                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-                    var disqus_shortname = 'trirussia'; // required: replace example with your forum shortname
+            <div class="card m-t-1">
+                <div class="card-block">
+                    <div id="disqus_thread"></div>
+                    <script type="text/javascript">
+                        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                        var disqus_shortname = 'trirussia'; // required: replace example with your forum shortname
 
-                    /* * * DON'T EDIT BELOW THIS LINE * * */
-                    (function() {
-                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-                    })();
-                </script>
-                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-                <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+                        /* * * DON'T EDIT BELOW THIS LINE * * */
+                        (function() {
+                            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                        })();
+                    </script>
+                    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 sidebar">
+            <?= \frontend\widgets\allRaces\AllRaces::widget(['raceView' => true, ]); ?>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 hidden new-sidebar">
+            <?= \frontend\widgets\allRaces\AllRaces::widget(['raceView' => true, ]); ?>
+            <div class="ad-sidebar text-xs-center">
+                <a href="https://www.asics.ru/running/products/gel-kayano-men/" target="_blank"><img src="http://files.www.fleetfeetraleigh.com/news/cq5dam.thumbnail.400.400-process-s400x333.png"></a>
             </div>
         </div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 sidebar">
-        <?= \frontend\widgets\allRaces\AllRaces::widget(['raceView' => true, ]); ?>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 hidden new-sidebar">
-        <?= \frontend\widgets\allRaces\AllRaces::widget(['raceView' => true, ]); ?>
-        <div class="ad-sidebar text-xs-center">
-            <a href="https://www.asics.ru/running/products/gel-kayano-men/" target="_blank"><img src="http://files.www.fleetfeetraleigh.com/news/cq5dam.thumbnail.400.400-process-s400x333.png"></a>
-        </div>
-    </div>
-</div>
 </div>
