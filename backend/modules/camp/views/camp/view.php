49981dce57e894cce3a371bcbfebb16ef554ace5
ritor <riiitor@gmail.com>
2017-05-17 12:23:33 +0300

    camp

--- backend/modules/camp/views/camp/view.php
+++ backend/modules/camp/views/camp/view.php
@@ -7,44 +7,61 @@ use yii\widgets\DetailView;
 /* @var $model camp\models\Camp */
 
 $this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Camps', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Кэмпы', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="camp-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+<section class="content-header race-view">
+    <?php
+    $breadcrumbs = isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [];
+    if ($breadcrumbs) {?>
+        <?= \yii\widgets\Breadcrumbs::widget(
+            [
+                'links' => $breadcrumbs,
+                'tag' => 'h1',
+                'itemTemplate' => "{link}\n <span>•</span> ",
+                'activeItemTemplate' => "<small>{link}</small>\n",
+                'options' => ['class' => '',]
+            ]
+        ) ?>
+    <?php } ?>
+</section>
+<section class="content camp-view">
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Редактировать', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Вы действительно хотите удалить этот объект?',
                 'method' => 'post',
             ],
         ]) ?>
     </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'label',
-            'url:url',
-            'country',
-            'region',
-            'place',
-            'coord_lon',
-            'coord_lat',
-            'date_start',
-            'date_end',
-            'max_user_count',
-            'promo:ntext',
-            'description:ntext',
-            'image_id',
-            'price',
-            'currency',
-        ],
-    ]) ?>
-
-</div>
+    <div class="row">
+        <div class="col-xs-9">
+            <div class="box box-primary">
+                <div class="box-body">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'id',
+                            'label',
+                            'url:url',
+                            'country',
+                            'region',
+                            'place',
+                            'coord_lon',
+                            'coord_lat',
+                            'date_start',
+                            'date_end',
+                            'max_user_count',
+                            'promo:ntext',
+                            'description:ntext',
+                            'image_id',
+                            'price',
+                            'currency'
+                    ]) ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
