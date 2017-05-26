beb5a2b79b808a76087077c037030ddf7b024df6
ritor <riiitor@gmail.com>
2017-05-26 15:47:41 +0300

    race form organizer

--- backend/modules/race/views/race/_form.php
+++ backend/modules/race/views/race/_form.php
@@ -336,7 +336,10 @@ $raceDistanceCount = count($raceDistanceArray);
                         'value' => $model->organizer ? $model->organizer->label : '',
                         'data' => ArrayHelper::map(Organizer::find()->orderBy(['label' => SORT_ASC])->all(), 'label', 'label'),
                         'theme' => Select2::THEME_KRAJEE,
-                        'options' => ['placeholder' => '-- Выберите организатора --'],
+                        'options' => [
+                            'placeholder' => '-- Выберите организатора --',
+                            'class' => 'form-control'
+                        ],
                         'pluginOptions' => [
                             'tags' => true,
                             'tokenSeparators' => [','],
