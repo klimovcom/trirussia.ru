17a0147d0327455706e70f250a95f79db5a6e926
ritor <riiitor@gmail.com>
2017-06-02 18:29:07 +0300

    race relay

--- frontend/modules/race/views/race-relay-registration/includes/relay_modal_cell_busy.php
+++ frontend/modules/race/views/race-relay-registration/includes/relay_modal_cell_busy.php
@@ -2,7 +2,7 @@
 use yii\helpers\Html;
 
 echo Html::tag('p', $registration->time, ['class' => 'm-b-0']);
-echo Html::tag('span', $registration->user->last_name . ' ' . $registration->user->first_name, ['class' => 'small underline']);
+echo Html::tag('span', $registration->user->last_name . ' ' . $registration->user->first_name, ['class' => 'small']);
 if (Yii::$app->user->id === $registration->user_id || Yii::$app->user->id === $first_user_id) {
     echo ' ' . Html::a(Html::tag('i', '', ['class' => 'fa fa-times', 'style' => 'color: #cc0000;']), 'javascript:;', [
             'class' => 'btn-race-relay-modal-unregister',
