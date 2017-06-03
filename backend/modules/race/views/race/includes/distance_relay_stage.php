0884fb2d4d1cedb9b5bcd8c0528195cf5e88a971
ritor <riiitor@gmail.com>
2017-06-03 19:40:49 +0300

    race relay distance

--- backend/modules/race/views/race/includes/distance_relay_stage.php
+++ backend/modules/race/views/race/includes/distance_relay_stage.php
@@ -10,7 +10,7 @@ $distance_formname = 'RaceDistanceRef[' . $distance_counter .'][relay][' . $rela
         <?= Html::dropDownList($distance_formname . '[sport]', $model->sport, RaceRelay::getSportArray(), ['class' => 'form-control']);?>
     </div>
     <div class="form-group">
-        <div class="control-label">Дистанция (км)</div>
+        <div class="control-label">Дистанция (м)</div>
         <?= Html::input('text', $distance_formname . '[distance]', $model->distance, ['class' => 'form-control']);?>
     </div>
     <div class="form-group">
