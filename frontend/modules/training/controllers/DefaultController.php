e780b43542ca960fbc57cf05398265fa8c5f9281
ritor <riiitor@gmail.com>
2017-06-16 14:38:08 +0300

    training

--- frontend/modules/training/controllers/DefaultController.php
+++ frontend/modules/training/controllers/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace training\controllers;
 
+use training\models\TrainingPlace;
 use Yii;
 use sport\models\Sport;
 use training\models\Training;
+use yii\db\Query;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 
@@ -45,7 +47,7 @@ class DefaultController extends Controller
      * Renders the index view for the module
      * @return string
      */
-    public function actionIndex($sport_name = null)
+    public function actionIndex($sport_name = null, $trainer = null, $place = null)
     {
 
         $query = Training::find()->where(['>', 'date', date('Y-m-d', time())]);
@@ -57,14 +59,63 @@ class DefaultController extends Controller
             }
         }
 
+        if ($trainer) {
+            $query->andWhere(['trainer_name' => $trainer]);
+        }
+
+        if ($place) {
+            $placeModel = TrainingPlace::find()->where(['label' => $place])->one();
+            if ($placeModel) {
+                $query->andWhere(['place_id' => $placeModel->id]);
+            }else {
+                $place = null;
+            }
+        }
+
         $models = $query->published()->orderBy(['date' => SORT_ASC])->all();
 
         $sports = Sport::find()->published()->all();
+
+        if (Yii::$app->cache->exists('training_authors')) {
+            $authors = Yii::$app->cache->get('training_authors');
+        }else {
+            $authors = (new Query())->
+                select(['trainer_name', 'COUNT(trainer_name) as training_count', 'date'])
+                ->from(Training::tableName())
+                ->where(['published' => 1])
+                ->andWhere(['>', 'date', date('Y-m-d', time())])
+                ->groupBy(['trainer_name'])
+                ->orderBy(['training_count' => SORT_DESC])
+                ->limit(10)
+                ->all();
+            Yii::$app->cache->set('training_authors', $authors, 60 * 60);
+        }
+
+        if (Yii::$app->cache->exists('training_places')) {
+            $places = Yii::$app->cache->get('training_places');
+        }else {
+            $places = (new Query())->
+                select(['place_id', 'COUNT(place_id) as place_count', TrainingPlace::tableName() .'.label', Training::tableName() . '.date'])
+                ->from(TrainingPlace::tableName())
+                ->leftJoin(Training::tableName(), TrainingPlace::tableName() . '.id =' . Training::tableName() . '.place_id')
+                ->where([Training::tableName() . '.published' => 1])
+                ->andWhere(['>', Training::tableName() . '.date', date('Y-m-d', time())])
+                ->groupBy(['place_id'])
+                ->orderBy(['place_count' => SORT_DESC])
+                ->limit(10)
+                ->all();
+            Yii::$app->cache->set('training_places', $places, 60 * 60);
+        }
+
 
         return $this->render('index', [
             'models' => $models,
             'sports' => $sports,
             'sport_name' => $sport_name,
+            'authors' => $authors,
+            'places' => $places,
+            'trainer' => $trainer,
+            'place' => $place,
         ]);
     }
 
--- frontend/modules/training/views/default/index.php
+++ frontend/modules/training/views/default/index.php
@@ -4,10 +4,55 @@ use yii\helpers\Html;
 
 $sportArray = array_merge(['all' => 'Все виды спорта'], ArrayHelper::map($sports, 'label', 'label'));
 ?>
-
+<div class="search-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <p><strong>Где проходят</strong></p>
+                <ul class="list-inline">
+                    <?php
+                    foreach ($authors as $item) {
+                        echo Html::tag('li', Html::a($item['trainer_name'], ['/training/default/index', 'trainer' => $item['trainer_name']], ['class' => 'underline']) . ' ' . Html::tag('span', $item['training_count'], ['class' => 'small text-muted']), ['class' => 'list-inline-item']);
+                    }
+                    ?>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <p><strong>Кто проводит</strong></p>
+                <ul class="list-inline">
+                    <?php
+                    foreach ($places as $item) {
+                        echo Html::tag('li', Html::a($item['label'], ['/training/default/index', 'place' => $item['label']], ['class' => 'underline']) . ' ' . Html::tag('span', $item['place_count'], ['class' => 'small text-muted']), ['class' => 'list-inline-item']);
+                    }
+                    ?>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div class="register m-a-0">
+                    <p class="text-muted">Если вы устраиваете тренировки, то добавьте её бесплатно.</p>
+                    <?php if(Yii::$app->user->isGuest) {
+                        echo Html::a('Добавить тренировку', ['/training/default/create'], ['class' => 'btn btn-secondary btn-sm', 'data-toggle' => 'modal', 'data-target' => '#openUser']);
+                    }else {
+                        echo Html::a('Добавить тренировку', ['/training/default/create'], ['class' => 'btn btn-secondary btn-sm']);
+                    }?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="container">
     <div class="pull-left">
-        <h1 class="m-y-3">Тренировки на велосипедах, по плаванию и бегу</h1>
+        <h1 class="m-y-3">
+            <?php
+            if ($trainer) {
+                echo 'Тренировки тренера ' . $trainer;
+            }elseif ($place) {
+                echo 'Тренировки в ' . $place;
+            }else {
+                echo 'Тренировки на велосипедах, по плаванию и бегу';
+            }
+            ?>
+        </h1>
     </div>
     <div class="clearfix"></div>
     <div class="card card-block">
